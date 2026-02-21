af6c8b7bf5b265e5786ddc4682b85d1a44a6931c
Bryce285 <bryceclark237@gmail.com>
2026-02-20 18:31:07 -0800

    Add some utility methods for AES key sharing

--- crypto.py
+++ crypto.py
@@ -12,13 +12,10 @@ from cryptography.hazmat.primitives import serialization
 
 from typing import Tuple
 
-"""
-TODO
-We need to decide on a protocol for sharing the symmetric key. This could be done
-by either writing the key to a usb for the user to plug into the other device, or
-we could just display the key as hex on the screen and have the user manually
-enter it into the other device.
-"""
+import os
+import subprocess
+import platform
+
 
 
 """
@@ -27,10 +24,10 @@ For AES symmetric encryption
 class Symmetric:
     """
     Checks if an AES key file exists and either loads the key from that file
-    or generates a new key and writes it to a file
+    or generates a new key and writes it to a file.
 
     Parameters: void
-    Returns: AES key bytes
+    Returns: AES key bytes on success, nothing on failure
     """
     @staticmethod
     def load_or_generate_key() -> bytes:
@@ -42,11 +39,19 @@ class Symmetric:
                     aes_key = aes_key_file.read()
             except FileNotFoundError:
                 print("aes.key file not found")
+                return
+            except OSError:
+                print("Failed to read AES key from aes.key")
+                return
         else:
             aes_key = Fernet.generate_key()
-
-            with open("aes.key", "wb") as aes_key_file:
-                aes_key_file.write(aes_key)
+            
+            try:
+                with open("aes.key", "wb") as aes_key_file:
+                    aes_key_file.write(aes_key)
+            except OSError:
+                print("Failed to write AES key to aes.key")
+                return
 
         return aes_key
 
@@ -79,7 +84,119 @@ class Symmetric:
     @staticmethod
     def decrypt_aes(ciphertext: bytes, fernet: Fernet) -> bytes:
         return fernet.decrypt(ciphertext)
+    
+    """
+    Checks if a file path leads to a USB storage device. This function is currently only implemented for Linux
+    because checking this information on Windows is a little more difficult.
 
+    Parameters: the path to check
+    Returns: true if the path leads to a USB storage device, false otherwise
+    """
+    @staticmethod
+    def is_usb_linux(path: str) -> Tuple[bool, str]:
+        if platform.system() != "Linux":
+            raise Exception("Storage device-type checking is only available for Linux systems.")
+        
+        path = os.path.abspath(path)
+
+        df_output = subprocess.check_output(["df", path]).decode().splitlines()
+        device = df_output[1].split()[0]
+
+        lsblk_output = subprocess.check_output(
+            ["lsblk", "-no", "TRAN", device]
+        ).decode().strip()
+
+        return lsblk_output == "usb", device
+
+    """
+    Formats a usb device. This function currently only available for Linux.
+
+    Parameters: the path of the usb device
+    Returns: true on success, false on failure
+    """
+    @staticmethod
+    def format_usb_linux(device_path: str) -> bool:
+        if platform.system() != "Linux":
+            raise Exception("USB formatting is only available for Linux systems.")
+
+        user_confirm = input("WARNING: This operation will erase all data on the selected USB storage device. Do you want to continue? (Y / n): ")
+        if user_confirm != "Y" or user_confirm != "y":
+            print("USB write operation cancelled.")
+            return False
+        
+        try:
+            subprocess.run(['sudo', 'umount', device_path], check=False)
+        except Exception:
+            pass
+
+        format_cmd = ['sudo', f'mkfs.ext4', '-F', device_path]
+
+        print(f"Attempting to format device {device_path} to ext4...")
+        try:
+            subprocess.run(
+                format_cmd, 
+                check=True, 
+                stdout=subprocess.PIPE, 
+                stderr=subprocess.PIPE
+            )
+            print("USB storage device formatted successfully.")
+            return True
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred: {e.stderr.decode()}")
+            return False
+        except Exception as e:
+            print(f"An unexpected error occured: {e}")
+            return False
+        
+        
+
+    """
+    Formats a USB storage device and then writes the AES key to it. We only allow writes to USB 
+    devices because we don't want users accidentally writing their keys to random directories. 
+    Because is_usb() is currently only implemented for Linux, this means that key writes can 
+    only be done on Linux. Non-Linux users should use the display_key() function to view their 
+    AES key from the program, where they can then manually copy it for sharing.
+
+    Parameters: the path to write to, and the key to write
+    Returns: true on success, false on failure
+    """
+    @staticmethod
+    def write_key_linux(path: str, key: bytes) -> bool:
+        if platform.system() != "Linux":
+            raise Exception("Writing AES keys to a file is only available for Linux systems.")
+        
+        is_usb, device_path = Symmetric.is_usb_linux(path)
+
+        if is_usb:
+            if not Symmetric.format_usb_linux:
+                print("Failed to format USB storage device.")
+                return False
+
+            try:
+                with open(path, "wb") as key_file:
+                    key_file.write(key)
+            except OSError:
+                print("Failed to write key to USB storage device")
+                return False
+        else:
+            print(path + " does not lead to a USB storage device.")
+            print("Write operation Failed")
+            return False
+        
+        return True
+    
+    """
+    Displays the AES key as hex.
+
+    Parameters: AES key as a bytes object
+    Returns: void
+    """
+    @staticmethod
+    def display_key(key: bytes):
+        print("THIS IS YOUR SECRET AES KEY. ONLY SHARE THIS KEY WITH PEOPLE YOU WANT TO BE ABLE TO READ YOUR MESSAGES.")
+        print(key.hex())
+
+            
 
 """
 For RSA asymmetric encryption
@@ -91,7 +208,7 @@ class Asymmetric:
     the private key.
 
     Parameters: void
-    Returns: two bytes objects, one of which is the private key, and the other is the public key
+    Returns: two bytes objects on success (private key and public key), and nothing on failure
     """
     @staticmethod
     def load_or_generate_key_pair() -> Tuple[rsa.RSAPrivateKey, rsa.RSAPublicKey]:
@@ -107,6 +224,10 @@ class Asymmetric:
                     )
             except FileNotFoundError:
                 print("rsa_private.key file not found")
+                return
+            except OSError:
+                print("Failed to read RSA private key from rsa_private.key")
+                return
         else:
             private_key = rsa.generate_private_key(
                 public_exponent=65537,
@@ -118,9 +239,12 @@ class Asymmetric:
                 format=serialization.PrivateFormat.PKCS8,
                 encryption_algorithm=serialization.NoEncryption()
             )
-
-            with open("rsa_private.key", "wb") as rsa_private_key_file:
-                rsa_private_key_file.write(pem_bytes)
+            try:
+                with open("rsa_private.key", "wb") as rsa_private_key_file:
+                    rsa_private_key_file.write(pem_bytes)
+            except OSError:
+                print("Failed to write RSA private key to rsa_private.key")
+                return
 
         public_key = private_key.public_key()
 
@@ -184,6 +308,8 @@ def main():
     decrypted_plaintext = Symmetric.decrypt_aes(ciphertext, fernet)
     print(f"Decrypted AES plaintext: {decrypted_plaintext.decode("utf-8")}")
 
+
+
     # testing asymmetric encryption
     private_key, public_key = Asymmetric.load_or_generate_key_pair()
 
@@ -195,6 +321,24 @@ def main():
 
     decrypted_plaintext = Asymmetric.decrypt_rsa(ciphertext, private_key)
     print(f"Decrypted RSA plaintext: {decrypted_plaintext.decode("utf-8")}")
+
+
+
+    # testing key display and USB writes
+    print("AES key (hex): ")
+    Symmetric.display_key(key)
+
+    user_in = input("Enter a USB device that you don't care about. Press 'Y' when it is inserted: ")
+    if user_in != "Y" or user_in != "y":
+        print("Exiting")
+        return
+    
+    user_in = input("Enter the path to the usb: ")
+    
+    if Symmetric.write_key_linux(user_in, key):
+        print("AES key successfully written to USB storage device.")
+    else:
+        print("Failed to write AES key to USB storage device.")
 
 if __name__ == "__main__":
     main()
---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+This file contains the main function for the program.
+"""
+
+import crypto
+
+"""
+Basic program loop. This can be implemented once we have message sending/receiving set up
+"""
+def main():
+    quit = False
+
+    while not quit:
+        pass
+
+if __name__ == "__main__":
+    main()
