0181fa4dce2598cbc5939a0b83a6b37d9cfea91e
Taiming <33017152-taimingyj@users.noreply.gitlab.com>
2026-03-02 20:09:17 -0800

    Add sounddevice and soundfile to pixi.toml via PyPI

--- src/main.py
+++ src/main.py
@@ -8,10 +8,7 @@ import crypto
 Basic program loop. This can be implemented once we have message sending/receiving set up
 """
 def main():
-    quit = False
-
-    while not quit:
-        pass
+    
 
 if __name__ == "__main__":
     main()
